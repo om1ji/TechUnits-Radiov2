f17ae8dd9b65215b767a14f15c723d29cd59a957
Амаль <om1ji.music@gmail.com>
2024-12-01 02:28:37 +0300

    change urls

--- my-radio/src/data/streams.js
+++ my-radio/src/data/streams.js
@@ -3,14 +3,14 @@ export const streams = [
         id: 1,
         name: 'DreamSystem',
         streamUrl: 'https://techunits.space/dreamsystem',
-        statsUrl: 'http://217.18.63.250:8000/status-json.xsl',
+        statsUrl: 'https://techunits.space/radio/status-json.xsl',
         backgroundImage: '/images/dreamsystem-bg.png'
     },
     {
         id: 2,
         name: 'Om1ji',
         streamUrl: 'https://techunits.space/om1ji',
-        statsUrl: 'http://217.18.63.250:8000/status-json.xsl',
+        statsUrl: 'https://techunits.space/radio/status-json.xsl',
         backgroundImage: '/images/om1ji-bg.png',
         logo: '/images/om1ji-logo.png'
     },
@@ -18,12 +18,12 @@ export const streams = [
         id: 3,
         name: 'snowdropborogoves',
         streamUrl: 'https://techunits.space/snowdropborogoves',
-        statsUrl: 'http://217.18.63.250:8000/status-json.xsl',
+        statsUrl: 'https://techunits.space/radio/status-json.xsl',
     },
     {
         id: 4,
         name: 'Alex Olson',
         streamUrl: 'https://techunits.space/alexolson',
-        statsUrl: 'http://217.18.63.250:8000/status-json.xsl',
+        statsUrl: 'https://techunits.space/radio/status-json.xsl',
     }
 ]; 
